ba5aeaba27d0749ace9772a422d59c6b6e8f8792
DanielSan2023 <electronic.san@gmail.com>
2023-12-17 15:33:09 +0100

    added  CustomerController

---
+++ src/test/java/com/projectDB/Shop/DBServiceTest.java
@@ -0,0 +1,104 @@
+package com.projectDB.Shop;
+
+import com.projectDB.Shop.db.service.api.CustomerService;
+import com.projectDB.Shop.db.service.api.MerchantService;
+import com.projectDB.Shop.db.service.api.ProductService;
+import com.projectDB.Shop.db.service.api.request.UpdateProductRequest;
+import com.projectDB.Shop.model.Customer;
+import com.projectDB.Shop.model.Merchant;
+import com.projectDB.Shop.model.Product;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)// cleanH2 DB
+public class DBServiceTest {
+
+    @Autowired
+    private CustomerService customerService;
+    @Autowired
+    private MerchantService merchantService;
+    @Autowired
+    private ProductService productService;
+
+    private static Merchant merchant;
+
+    @BeforeEach
+    public void createMerchant() {
+        if (merchant == null) {
+            merchant = new Merchant("name", "email", "address");
+            Integer id = merchantService.add(merchant);
+            assert id != null;
+            merchant.setId(id);
+        }
+    }
+
+    @Test
+    public void customer() {
+        Customer customer =
+                new Customer("Ferko", "Mrkvicka",
+                        "emailtest", "test", 20, "xxx");
+        Integer id = customerService.add(customer);
+        assert id != null;
+        customer.setId(id);
+
+        Customer fromDb = customerService.get(id);
+        Assertions.assertEquals(customer, fromDb);
+
+        List<Customer> customers = customerService.getCustomers();
+        Assertions.assertEquals(1, customers.size());
+        Assertions.assertEquals(customer, customers.get(0));
+    }
+
+
+    @Test
+    public void merchant() {
+        // already created merchant in createMerchant function
+
+        Merchant fromDB = merchantService.get(merchant.getId());
+        Assertions.assertEquals(merchant, fromDB);
+
+        List<Merchant> merchants = merchantService.getMerchants();
+        Assertions.assertEquals(1, merchants.size());
+        Assertions.assertEquals(merchant, merchants.get(0));
+    }
+
+    @Test
+    public void product() {
+        Product product = new Product(merchant.getId(), "name", "description", 5, 1);
+        Integer id = productService.add(product);
+        assert id != null;
+        product.setId(id);
+
+        Product fromDB = productService.get(id);
+        Assertions.assertEquals(product, fromDB);
+
+        List<Product> products = productService.getProducts();
+        Assertions.assertEquals(1, products.size());
+        Assertions.assertEquals(product, products.get(0));
+
+        product.setAvailable(10);
+        UpdateProductRequest productRequest = new UpdateProductRequest(
+                product.getName(), product.getDescription(), product.getPrice(), product.getAvailable());
+
+        productService.update(id, productRequest);
+        Product fromDBAfterUpdate = productService.get(id);
+        Assertions.assertEquals(product, fromDBAfterUpdate);
+        Assertions.assertNotEquals(fromDB, fromDBAfterUpdate);
+
+        productService.delete(id);
+        Assertions.assertEquals(0, productService.getProducts().size());
+    }
+
+
+}
